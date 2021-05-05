3e01fa1a37ccd5ee8718ed5d03675f1a1f97641f
Paweł Łakomiec <pawellakomiec95@gmail.com>
2021-05-05 22:44:00 +0200

    no message

---
+++ gym_gymbjpl/__init__.py
@@ -0,0 +1,6 @@
+from gym.envs.registration import register
+
+register(
+    id='gymbjpl-v0',
+    entry_point='gym_foo.envs:GymbjplEnv',
+)
---
+++ gym_gymbjpl/envs/__init__.py
@@ -0,0 +1,1 @@
+from gym_gymbjpl.envs.gymbjpl_env import GymbjplEnv
